acc9b4762d253358fde1b74c10763103c976aee4
darfux <darfux@163.com>
2016-10-28 16:42:40 +0800

    fixes

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   get 'main/index'
 
-  post 'main/upload'
+  post 'main/upload', as: 'upload'
 
   get 'login', to: "user/session#new", as: 'login'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
